25817d965ed3d816d9589a9c5916e8569e9b9ba8
Nazmi17 <ramadaninazmi47@gmail.com>
2025-06-21 23:06:34 +0700

    perbaikan

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\productController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::get('/todos', [TodoController::class, 'index']);
 Route::post('/todos', [TodoController::class, 'store']);
